c8ecc31d7378a97cca3d9fed11a61e35fb7728a6
ChangNing <ningchang823@gmail.com>
2018-09-02 18:10:51 +0800

    Update 32_LongestValidParentheses.cpp

--- 32_LongestValidParentheses.cpp
+++ 32_LongestValidParentheses.cpp
@@ -2,27 +2,16 @@ class Solution {
 public:
     int longestValidParentheses(string s) {
         int n = s.size();
-        stack<pair<char, int>> st;
-        int *valid = (int *)malloc(sizeof(int)*n);
-        memset(valid, 0, sizeof(int)*n);
-        
+        int max = 0;
+        stack<int> st;
+        st.push(-1);
         for (int i=0; i<n; i++){
-            if (s[i] == '(')
-                st.push(pair<char,int>(s[i], i));
-            else if (!st.empty() && st.top().first == '('){
-                valid[st.top().second] = 1;
-                valid[i] = 1;
+            if (s[i] == '(') st.push(i);
+            else {
                 st.pop();
+                if (st.empty()) st.push(i);
+                else max = i-st.top()>max? i-st.top(): max;
             }
-        }
-        int max = 0, len = 0;
-        for (int i=0; i<n; i++){
-            if (valid[i]){
-                len++;
-                if (len > max) max = len;
-            }
-            else
-                len = 0;
         }
         return max;
     }
